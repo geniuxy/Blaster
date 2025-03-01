386c508d9e0288fbc254471b8ade394683781319
星晔 邱 <2238384910@qq.com>
2025-03-01 19:30:25 +0800

    [25.3.1](159) Pickup Class

---
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
+APickup::APickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SetRootComponent(PickupMesh);
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	OverlapSphere->SetupAttachment(GetRootComponent());
+	OverlapSphere->SetSphereRadius(150.f);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+}
+
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APickup::Destroyed()
+{
+	Super::Destroyed();
+
+	if (PickupSound)
+		UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+}
+
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                              UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                              const FHitResult& SweepResult)
+{
+}
