5f27db0247a98ed1332c621c90a7cdd17c451358
星晔 邱 <2238384910@qq.com>
2025-03-02 00:33:20 +0800

    [25.3.2](162) Health Pickup

---
+++ Source/Blaster/Pickups/HealthPickup.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Blaster/Character/BlasterCharacter.h"
+
+
+AHealthPickup::AHealthPickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(GetRootComponent());
+}
+
+void AHealthPickup::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                    const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+	}
+	Destroy();
+}
+
+void AHealthPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupEffect)
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, PickupEffect, GetActorLocation(), GetActorRotation());
+
+	Super::Destroyed();
+}
---
+++ Source/Blaster/Pickups/HealthPickup.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "HealthPickup.generated.h"
+
+class UNiagaraSystem;
+class UNiagaraComponent;
+
+UCLASS()
+class BLASTER_API AHealthPickup : public APickup
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPickup();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Destroyed() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float HealAmount = 100.f;
+
+	UPROPERTY(EditAnywhere)
+	float HealingTime = 5.f;
+	
+	UPROPERTY(VisibleAnywhere)
+	UNiagaraComponent* PickupEffectComponent;
+
+	UPROPERTY(EditAnywhere)
+	UNiagaraSystem* PickupEffect;
+	
+
+public:
+};
