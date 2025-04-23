075b87d3914ec23c2d63288ba6f3a35db0a90afe
geniuxy <2238384910@qq.com>
2025-04-23 20:03:55 +0800

    [25.4.23](202) Post Edit Change Property

--- Source/Blaster/Weapon/ProjectileBullet.cpp
+++ Source/Blaster/Weapon/ProjectileBullet.cpp
@@ -16,6 +16,24 @@ AProjectileBullet::AProjectileBullet()
 	ProjectileMovementComponent->MaxSpeed = InitialSpeed;
 }
 
+#if WITH_EDITOR
+void AProjectileBullet::PostEditChangeProperty(FPropertyChangedEvent& Event)
+{
+	Super::PostEditChangeProperty(Event);
+
+	// GetFName()  ...
+	FName PropertyName = Event.Property != nullptr ? Event.Property->GetFName() : NAME_None;
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(AProjectileBullet, InitialSpeed))
+	{
+		if (ProjectileMovementComponent)
+		{
+			ProjectileMovementComponent->InitialSpeed = InitialSpeed;
+			ProjectileMovementComponent->MaxSpeed = InitialSpeed;
+		}	
+	}
+}
+#endif
+
 void AProjectileBullet::BeginPlay()
 {
 	Super::BeginPlay();
