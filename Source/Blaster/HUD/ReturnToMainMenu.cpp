b4018deb238fbdd7d59d56ea1868421c3b4d3cc1
geniuxy <2238384910@qq.com>
2025-04-25 01:16:34 +0800

    [25.4.25](212) Return to Main Menu

---
+++ Source/Blaster/HUD/ReturnToMainMenu.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReturnToMainMenu.h"
+
+#include "MultiplayerSessionsSubsystem.h"
+#include "Components/Button.h"
+#include "GameFramework/GameModeBase.h"
+
+void UReturnToMainMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	SetIsFocusable(true);
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		PlayerController = PlayerController == nullptr ? World->GetFirstPlayerController() : PlayerController;
+		if (PlayerController)
+		{
+			FInputModeGameAndUI InputModeData;
+			InputModeData.SetWidgetToFocus(TakeWidget());
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
+
+	if (ReturnButton)
+		ReturnButton->OnClicked.AddDynamic(this, &UReturnToMainMenu::ReturnButtonClicked);
+
+	UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance)
+	{
+		MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+		if (MultiplayerSessionsSubsystem)
+		{
+			MultiplayerSessionsSubsystem->
+				MultiplayerOnDestroySessionDelegate.AddDynamic(this, &UReturnToMainMenu::OnDestroySession);
+		}
+	}
+}
+
+void UReturnToMainMenu::MenuTearDown()
+{
+	RemoveFromParent();
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		PlayerController = PlayerController == nullptr ? World->GetFirstPlayerController() : PlayerController;
+		if (PlayerController)
+		{
+			FInputModeGameOnly InputModeData;
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(false);
+		}
+	}
+}
+
+void UReturnToMainMenu::OnDestroySession(bool bWasSuccessful)
+{
+	if (!bWasSuccessful)
+	{
+		ReturnButton->SetIsEnabled(true);
+		return;
+	}
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		AGameModeBase* GameMode = World->GetAuthGameMode<AGameModeBase>();
+		if (GameMode) // 如果是主机，就都别玩了
+		{
+			GameMode->ReturnToMainMenuHost();
+		}
+		else // 如果是client，就只退一个
+		{
+			PlayerController = PlayerController == nullptr ? World->GetFirstPlayerController() : PlayerController;
+			if (PlayerController)
+			{
+				PlayerController->ClientReturnToMainMenuWithTextReason(FText());
+			}
+		}
+	}
+}
+
+void UReturnToMainMenu::ReturnButtonClicked()
+{
+	ReturnButton->SetIsEnabled(false);
+
+	if (MultiplayerSessionsSubsystem)
+		MultiplayerSessionsSubsystem->DestroySession();
+}
---
+++ Source/Blaster/HUD/ReturnToMainMenu.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ReturnToMainMenu.generated.h"
+
+class UMultiplayerSessionsSubsystem;
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API UReturnToMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void MenuSetup();
+	void MenuTearDown();
+
+protected:
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UButton* ReturnButton;
+
+	UFUNCTION()
+	void ReturnButtonClicked();
+
+	UPROPERTY()
+	UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
+
+	UPROPERTY()
+	APlayerController* PlayerController;
+};
